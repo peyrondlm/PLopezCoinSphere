5bb0a5ee1d0a941c54d81502c0ab95b379a654a2
Pedro Lopez <peyron.dlm@gmail.com>
2025-09-11 20:15:44 -0600

    Creando la parte para las monedas

---
+++ app/src/main/java/com/example/coinsphere/DataClasses/CoinModel.kt
@@ -0,0 +1,7 @@
+package com.example.coinsphere.DataClasses
+
+data class CoinModel(
+    val coinName : String,
+    val coinImage : String,
+    val coinPrice : Double
+)
--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,6 +27,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.coinsphere.DataClasses.CoinModel
 import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
 import com.example.coinsphere.ui.theme.Surface
@@ -45,11 +50,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
+    // Monedas
+    val coins = listOf(
+        CoinModel("Bitcoin", "asdf", 109797.37),
+        CoinModel("Ethereum", "asdf", 4321.21),
+        CoinModel("Tether", "asdf", 1.0000)
+
+    )
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Background)
     ) {
+        // Nombre de la aplicación
         Row(
             modifier = Modifier
                 .padding(start = 15.dp, top = 30.dp, end = 15.dp, bottom = 5.dp)
@@ -61,6 +74,7 @@ fun HomeScreen() {
                 fontSize = 20.sp
             )
         }
+        // Información útil
         Row (
             modifier = Modifier
                 .fillMaxWidth()
@@ -140,6 +154,53 @@ fun HomeScreen() {
                     fontWeight = FontWeight.SemiBold,
                     fontSize = 20.sp
                 )
+            }
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, top = 25.dp, end = 20.dp, bottom = 5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = "#   Name",
+                color = TextDim,
+                fontSize = 14.sp
+            )
+            Text(
+                text = "Price",
+                color = TextDim,
+                fontSize = 14.sp
+            )
+        }
+        // Línea que separa el nombre y precio de los elementos
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(3.dp)
+                .background(Surface)
+        )
+        // Monedas
+        LazyColumn (
+
+        ){
+            items(items = coins) { coin ->
+                Row (
+                    modifier = Modifier
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Row {
+                        Text(
+                            text = coin.coinName
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = "$${coin.coinPrice}"
+                        )
+                    }
+                }
             }
         }
     }
