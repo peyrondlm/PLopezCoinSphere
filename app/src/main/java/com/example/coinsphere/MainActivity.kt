c91a3e27328c7627ddb1d89dcee3cf216438b359
Pedro Lopez <peyron.dlm@gmail.com>
2025-09-11 19:44:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.example.coinsphere
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.coinsphere.ui.theme.Background
+import com.example.coinsphere.ui.theme.CoinSphereTheme
+import com.example.coinsphere.ui.theme.Surface
+import com.example.coinsphere.ui.theme.TextDim
+import com.example.coinsphere.ui.theme.TextMain
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CoinSphereTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(start = 15.dp, top = 30.dp, end = 15.dp, bottom = 5.dp)
+        ) {
+            Text(
+                text = "CoinSphere",
+                color = TextMain,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 12.dp, end = 15.dp)
+                .clip(RoundedCornerShape(18.dp))
+                .background(Surface)
+        ) {
+            Column (
+                modifier = Modifier
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+
+            ) {
+                Text(
+                    text = "Global Market Cap",
+                    color = TextDim,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp
+                )
+                Text(
+                    text = "$2.18T",
+                    color = TextMain,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 12.dp, end = 15.dp)
+                .clip(RoundedCornerShape(18.dp))
+                .background(Surface)
+        ) {
+            Column (
+                modifier = Modifier
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+
+            ) {
+                Text(
+                    text = "Fear & Greed",
+                    color = TextDim,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp
+                )
+                Text(
+                    text = "Neutral (54)",
+                    color = TextMain,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 15.dp, top = 12.dp, end = 15.dp)
+                .clip(RoundedCornerShape(18.dp))
+                .background(Surface)
+        ) {
+            Column (
+                modifier = Modifier
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+
+            ) {
+                Text(
+                    text = "Altcoin Season",
+                    color = TextDim,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp
+                )
+                Text(
+                    text = "No",
+                    color = TextMain,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+
+@Composable
+fun HomeScreenPreview() {
+    CoinSphereTheme {
+        HomeScreen()
+    }
+}
