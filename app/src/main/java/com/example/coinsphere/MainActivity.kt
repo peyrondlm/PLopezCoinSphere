0677a433f533e3087588bf6571282400ab5f83b3
Pedro Lopez <peyron.dlm@gmail.com>
2025-09-13 22:29:30 -0600

    Listo para segmentar

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -13,18 +13,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.Image
+import coil3.compose.AsyncImage
 import com.example.coinsphere.DataClasses.CoinModel
 import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
@@ -51,18 +56,18 @@ class MainActivity : ComponentActivity() {
 fun HomeScreen() {
     // Monedas
     val coins = listOf(
-        CoinModel("Bitcoin", "asdf", 109797.37),
-        CoinModel("Ethereum", "asdf", 4321.21),
-        CoinModel("Tether", "asdf", 1.0000),
-        CoinModel("XRP", "asdf", 2.8100),
-        CoinModel("BNB", "asdf", 845.0000),
-        CoinModel("Solana", "asdf", 201.8500),
-        CoinModel("USDC", "asdf", 0.9998),
-        CoinModel("Dogecoin", "asdf", 0.1320),
-        CoinModel("TRON", "asdf", 0.3630),
-        CoinModel("Cardano", "asdf", 0.9213),
-        CoinModel("Litecoin", "asdf", 117.78),
-        CoinModel("Chainlink", "asdf", 25.09)
+        CoinModel("Bitcoin", "https://cdn-icons-png.flaticon.com/128/5968/5968260.png", 109797.37),
+        CoinModel("Ethereum", "https://cdn-icons-png.flaticon.com/128/15301/15301597.png", 4321.21),
+        CoinModel("Tether", "https://cdn-icons-png.flaticon.com/128/12114/12114244.png", 1.0000),
+        CoinModel("XRP", "https://cdn-icons-png.flaticon.com/128/6675/6675835.png", 2.8100),
+        CoinModel("BNB", "https://cdn-icons-png.flaticon.com/128/12114/12114208.png", 845.0000),
+        CoinModel("Solana", "https://cdn-icons-png.flaticon.com/128/6001/6001527.png", 201.8500),
+        CoinModel("USDC", "https://cdn-icons-png.flaticon.com/128/14446/14446284.png", 0.9998),
+        CoinModel("Dogecoin", "https://cdn-icons-png.flaticon.com/128/7320/7320825.png", 0.1320),
+        CoinModel("TRON", "https://cdn-icons-png.flaticon.com/128/12114/12114250.png", 0.3630),
+        CoinModel("Cardano", "https://cdn-icons-png.flaticon.com/128/7016/7016514.png", 0.9213),
+        CoinModel("Litecoin", "https://cdn-icons-png.flaticon.com/128/3938/3938179.png", 117.78),
+        CoinModel("Chainlink", "https://cdn-icons-png.flaticon.com/128/6557/6557085.png", 25.09)
     )
     Column(
         modifier = Modifier
@@ -190,30 +195,48 @@ fun HomeScreen() {
         // Monedas
         LazyColumn (
             modifier = Modifier
-                .padding(start = 20.dp, top = 5.dp, end = 20.dp, bottom = 20.dp)
+                .padding(start = 12.dp, top = 5.dp, end = 12.dp, bottom = 20.dp)
         ){
             items(items = coins.withIndex().toList()) { (index, coin) ->
                 Row (
                     modifier = Modifier
-                        .padding(10.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                        .fillMaxWidth()
+                        .padding(start = 8.dp, top = 12.dp, end = 8.dp)
+                        .clip(RoundedCornerShape(20.dp))
+                        .background(Surface),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ){
                     Row (
                         modifier = Modifier
-                            .weight(1f)
-                    ){
+                            .padding(20.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         Text(
                             text = "${index + 1}",
                             color = TextMain,
                             modifier = Modifier
                                 .padding(end = 10.dp)
                         )
+                        AsyncImage(
+                            model = coin.coinImage,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .height(30.dp),
+                            contentScale = ContentScale.Fit
+                        )
                         Text(
                             text = coin.coinName,
-                            color = TextMain
+                            color = TextMain,
+                            modifier = Modifier
+                                .padding(start = 10.dp)
                         )
                     }
-                    Row {
+                    Row (
+                        modifier = Modifier
+                            .padding(20.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         Text(
                             text = "$${coin.coinPrice}",
                             color = TextMain
